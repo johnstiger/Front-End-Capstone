5c9ae8192907ea7d4874f373dcc88c4cee2451bb
johnstiger <johnstiger12@gmail.com>
2021-09-29 18:29:49 +0800

    [EDIT] try the new way in updating data

--- src/app/Admin/Components/products/edit-product/edit-product.component.ts
+++ src/app/Admin/Components/products/edit-product/edit-product.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { AdminService } from 'src/app/Admin/Services/admin.service';
 import { Product } from 'src/app/Admin/Common/model/admin-model';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-product',
@@ -12,13 +13,15 @@ import { Product } from 'src/app/Admin/Common/model/admin-model';
 export class EditProductComponent implements OnInit {
 
   imageSrc! : string;
-  product! : Product[];
-  image! : string;
-  name! : string;
-  category_id! : number;
-  price! : number;
-  sizes! : string;
-  unit_measure! : number;
+  UpdateProductForm = new FormGroup({
+    name: new FormControl(''),
+    image: new FormControl(''),
+    fileSource : new FormControl(''),
+    category_id: new FormControl(''),
+    price: new FormControl(''),
+    sizes: new FormControl(''),
+    unit_measure: new FormControl('')
+  })
 
 
   token = localStorage.getItem('admin_token');
@@ -46,9 +49,17 @@ export class EditProductComponent implements OnInit {
   async getProduct(){
     const result = await this.http.getProduct(this.token, this.id);
     if(result.data.error){
-
     }else{
-      this.product = result.data.data;
+      this.UpdateProductForm = new FormGroup({
+        name: new FormControl(result.data.data.name),
+        image: new FormControl(result.data.data.image),
+        fileSource : new FormControl(''),
+        category_id: new FormControl(''),
+        price: new FormControl(result.data.data.price),
+        sizes: new FormControl(result.data.sizes[0].size),
+        unit_measure: new FormControl(result.data.sizes[0].pivot.unit_measure)
+      })
+
     }
 
   }
@@ -57,7 +68,7 @@ export class EditProductComponent implements OnInit {
 
   }
 
-  async submit(any : any){
+  async submit(){
 
   }
 
