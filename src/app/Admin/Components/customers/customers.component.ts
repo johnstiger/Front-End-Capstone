2e18593b4890a2f72a22588f89ea52370330728b
2ndyrGroupB <2ndyrGroupB@pn.local>
2021-11-07 11:51:18 +0800

    Changes added today Nov 7

--- src/app/Admin/Components/customers/customers.component.ts
+++ src/app/Admin/Components/customers/customers.component.ts
@@ -53,4 +53,15 @@ export class CustomersComponent implements OnInit {
     }
 
   }
+
+  // searchFilter() {
+  //   $(document).ready(function(){
+  //     $("#myInput").on("keyup", function() {
+  //       var value = $(this).val().toLowerCase();
+  //       $("#myTable tr").filter(function() {
+  //         $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+  //       });
+  //     });
+  //   });
+  // }
 }
