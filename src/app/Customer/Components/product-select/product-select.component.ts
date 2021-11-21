d7db5f05cda1efa6b8702b9cc5904de6af52c66d
johnstiger12@gmail.com <sernel.asunto@student.passerellesnumeriques.org>
2021-11-21 22:02:00 +0800

    Changes Nov 21

--- src/app/Customer/Components/product-select/product-select.component.ts
+++ src/app/Customer/Components/product-select/product-select.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { CustomerService } from '../../Services/customer.service';
 import { Categories, Products, Sizes } from 'src/app/Customer/Common/model/customer-model';
 import { ActivatedRoute } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-product-select',
@@ -46,15 +47,17 @@ export class ProductSelectComponent implements OnInit {
       this.product = result.data.data;
       this.image =this.product.image;
       this.id = this.product.id;
-      this.avail_unit_measure = this.product.sizes[0].pivot.avail_unit_measure;
-      if (this.product.sizes.length < 0) {
+      if (this.product.sizes.length > 0) {
+        this.avail_unit_measure = this.product.sizes[0].pivot.avail_unit_measure;
         this.sizes = this.product.sizes[0].pivot.size;
         this.unit_measure = this.product.sizes[0].pivot.unit_measure;
       } else {
+        this.avail_unit_measure=0;
         this.sizes="";
         this.unit_measure = 0;
       }
     })
+    // this.service.closeLoading();
   }
 
   async submit(id:any, data:any) {
